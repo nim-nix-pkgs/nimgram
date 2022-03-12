396bc5c7e0786eef56c6575989146fa2b15097d3
hugosenari <hugosenari@gmail.com>
2022-03-12 04:27:53 -0300

    chore: re index nimgram

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''MTProto client written in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimgram-master".url = "path:./master";
+  inputs."nimgram-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgram-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimgram-v0_1_0".url = "path:./v0_1_0";
+  inputs."nimgram-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgram-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
